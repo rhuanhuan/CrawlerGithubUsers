d52b923cd571e16449ffe662d75be70c99ca51b1
rhuanhuan <1210098674@qq.com>
2018-04-02 21:12:45 +0800

    [Huan] Format code with google style, extract page index

--- CrawlerGithubUsers/spiders/github_spider.py
+++ CrawlerGithubUsers/spiders/github_spider.py
@@ -4,22 +4,30 @@ from CrawlerGithubUsers.items import GitUsersItem
 import os
 
 
-class GitHubSpider(CrawlSpider):
-  http_user = os.environ['USER_NAME']
-  http_pass = os.environ['USER_TOKEN']
-  name = "git_spider"
-  allowed_domains = ["github.com"]
-  start_urls = [
-    "https://api.github.com/search/users?q=+location:%22Wu%20Han%22&page=1&per_page=100",
-    "https://api.github.com/search/users?q=+location:%22WuHan%22&page=1&per_page=100"
-  ]
+def generate_url(page_index):
+    return "https://api.github.com/search/users" \
+           "?q=+location:%22Wu%20Han%22&page=" \
+           + page_index + \
+           "&per_page=100"
 
-  def parse(self, response):
-    for users in json.loads(response.body)["items"]:
-      item = GitUsersItem()
-      item['username'] = users["login"]
-      # item['name'] = users["name"]
-      item['link'] = users["html_url"]
-      # item['email'] = users["email"]
-      item['followers'] = users["followers_url"]
-      yield item
+
+class GitHubSpider(CrawlSpider):
+    http_user = os.environ['USER_NAME']
+    http_pass = os.environ['USER_TOKEN']
+    name = "git_spider"
+    allowed_domains = ["github.com"]
+    start_urls = [
+        generate_url(1),
+        "https://api.github.com/search/users?"
+        "q=+location:%22WuHan%22&page=1&per_page=100"
+    ]
+
+    def parse(self, response):
+        for users in json.loads(response.body)["items"]:
+            item = GitUsersItem()
+            item['username'] = users["login"]
+            # item['name'] = users["name"]
+            item['link'] = users["html_url"]
+            # item['email'] = users["email"]
+            item['followers'] = users["followers_url"]
+            yield item
