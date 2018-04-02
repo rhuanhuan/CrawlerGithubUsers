14cd002d9fe49e3965def1607e598755253446b7
rhuanhuan <1210098674@qq.com>
2018-04-02 23:29:03 +0800

    [Huan] Make it to continues catch user info by date. Since one search can return at most 1000 result

--- CrawlerGithubUsers/spiders/github_spider.py
+++ CrawlerGithubUsers/spiders/github_spider.py
@@ -1,19 +1,30 @@
 import json
+
+import scrapy
 from scrapy.contrib.spiders import CrawlSpider
 from CrawlerGithubUsers.items import GitUsersItem
 import os
 import requests
+import dateutil.parser
 
 
-def get_users_list_url(page_index):
+git_init_date = '2008-03-01T00:00:00Z'
+crawler_ended_date = '2018-03-25T00:00:00Z'
+
+
+def get_users_list_url(date):
     return "https://api.github.com/search/users" \
-           "?q=+location:%22Wu%20Han%22&page=" \
-           + str(page_index) + \
-           "&per_page=100"
+           "?q=+location:%22WuHan%22+created:>" + date + \
+           "&page=1" \
+           "&per_page=100&sort=joined&order=asc"
 
 
 def get_user_detail_url(username):
     return "https://api.github.com/users/" + username
+
+
+def get_timestamp(iso_time):
+    return dateutil.parser.parse(iso_time)
 
 
 class GitHubSpider(CrawlSpider):
@@ -21,14 +32,11 @@ class GitHubSpider(CrawlSpider):
     http_pass = os.environ['USER_TOKEN']
     name = "git_spider"
     allowed_domains = ["github.com"]
-    start_urls = [
-        get_users_list_url(1)
-        # "https://api.github.com/search/users?"
-        # "q=+location:%22WuHan%22&page=1&per_page=100"
-    ]
+    start_urls = [get_users_list_url(git_init_date)]
 
     def parse(self, response):
         json_body = json.loads(response.body)
+        last_created = git_init_date
         for users in json_body["items"]:
             item = GitUsersItem()
             item['username'] = users["login"]
@@ -46,4 +54,9 @@ class GitHubSpider(CrawlSpider):
             item['followers'] = user_detail["followers"]
             item['created_at'] = user_detail["created_at"]
             item['product'] = user_detail["public_repos"]
+            if get_timestamp(last_created) < get_timestamp(item['created_at']):
+                last_created = item['created_at']
             yield item
+        if get_timestamp(last_created) < get_timestamp(crawler_ended_date):
+            yield scrapy.Request(url=get_users_list_url(last_created),
+                                 callback=self.parse)
